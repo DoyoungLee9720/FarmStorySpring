c2bdc3f51a84a840f0f0df6bc530635a6d51cae1
DoyoungLee <dlehdud224@gmail.com>
2024-10-03 19:18:26 +0900

    ver 0.0.5T

--- src/test/java/com/farmstory/FarmStoryApplicationTests.java
+++ src/test/java/com/farmstory/FarmStoryApplicationTests.java
@@ -1,13 +1,30 @@
 package com.farmstory;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FarmStoryApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))  // 홈 페이지에 GET 요청
+				.andExpect(status().isOk())  // HTTP 상태가 200 OK인지 확인
+				.andExpect(view().name("home"));  // 응답 뷰 이름이 "home"인지 확인
+	}
 }
